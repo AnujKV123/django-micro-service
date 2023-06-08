a8ad06807f09c915c8948af31c4bb38c557602da
Anuj <anuj8ranchi@gmail.com>
2023-06-08 17:21:23 +0530

    added workflow file

--- docker_test/settings.py
+++ docker_test/settings.py
@@ -76,23 +76,23 @@ WSGI_APPLICATION = 'docker_test.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-DATABASES={
-   'default':{
-      'ENGINE':'django.db.backends.postgresql',
-      'NAME':os.getenv('DATABASE_NAME'),
-      'USER':os.getenv('DATABASE_USER'),
-      'PASSWORD':os.getenv('DATABASE_PASSWORD'),
-      'HOST':os.getenv('DATABASE_HOST'),
-      'PORT':'5432',
-   }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+# DATABASES={
+#    'default':{
+#       'ENGINE':'django.db.backends.postgresql',
+#       'NAME':os.getenv('DATABASE_NAME'),
+#       'USER':os.getenv('DATABASE_USER'),
+#       'PASSWORD':os.getenv('DATABASE_PASSWORD'),
+#       'HOST':os.getenv('DATABASE_HOST'),
+#       'PORT':'5432',
+#    }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
